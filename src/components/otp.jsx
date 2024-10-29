3e85d864454267ea983b625be902274bee9796ac
Shreyanshu005 <shreyanshu23153035@akgec.ac.in>
2024-10-29 23:30:19 +0530

    created otp page

---
+++ src/components/otp.jsx
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom';
+
+import React from 'react';
+import './css/otp.css';
+import bro1 from '../assets/pana.svg';
+
+const Otp = () => {
+
+
+    return(
+        <div className='otpPage'>
+        <div className='otpleft'>
+            <div className="otpleftSub">
+            <h2 id="otph2">Verify Your Email</h2>
+            <p id="otpp">We’ve sent a 6-digit verification code to xxx@gmail.com </p>
+    <form className="digits">
+    
+    <input type="text" id='digit1' />
+    <input type="text" id="digit2" />
+    <input type="text" id="digit3" />
+    <input type="text" id="digit4" />
+    <input type="text" id="digit5"  />
+    <input type="text" id="digit6" />
+   
+           
+            
+        
+    </form>
+    <p id='resend'>Resend OTP in 27s</p>
+           
+            
+            
+          
+           
+           
+        
+            <input type="submit" id='otpsubmit'value="Send reset link" />
+            <div className="askSign"> <p>Back to  <Link to="/signup">Sign Up</Link></p></div>
+           
+           
+            </div>
+      
+        </div>
+        <div className="otpright">
+         <div className="otpcontent">
+          
+          <img src={bro1} alt="" />
+    
+          
+    
+       
+         </div>
+         
+    
+        </div>
+      </div>
+    )
+}
+export default Otp;
